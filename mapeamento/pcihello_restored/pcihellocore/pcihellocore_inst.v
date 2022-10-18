81664dc5d2316c5f58e724146d26ab58b2d4d41f
Júnior Marcondes <marc@mm.local>
2022-10-18 11:03:39 -0300

    added mapeamento

---
+++ mapeamento/pcihello_restored/pcihellocore/pcihellocore_inst.v
@@ -0,0 +1,15 @@
+	pcihellocore u0 (
+		.pcie_hard_ip_0_rx_in_rx_datain_0       (<connected-to-pcie_hard_ip_0_rx_in_rx_datain_0>),       //            pcie_hard_ip_0_rx_in.rx_datain_0
+		.pcie_hard_ip_0_tx_out_tx_dataout_0     (<connected-to-pcie_hard_ip_0_tx_out_tx_dataout_0>),     //           pcie_hard_ip_0_tx_out.tx_dataout_0
+		.pcie_hard_ip_0_powerdown_pll_powerdown (<connected-to-pcie_hard_ip_0_powerdown_pll_powerdown>), //        pcie_hard_ip_0_powerdown.pll_powerdown
+		.pcie_hard_ip_0_powerdown_gxb_powerdown (<connected-to-pcie_hard_ip_0_powerdown_gxb_powerdown>), //                                .gxb_powerdown
+		.pcie_hard_ip_0_refclk_export           (<connected-to-pcie_hard_ip_0_refclk_export>),           //           pcie_hard_ip_0_refclk.export
+		.pcie_hard_ip_0_pcie_rstn_export        (<connected-to-pcie_hard_ip_0_pcie_rstn_export>),        //        pcie_hard_ip_0_pcie_rstn.export
+		.hexdisplay_external_connection_export  (<connected-to-hexdisplay_external_connection_export>),  //  hexdisplay_external_connection.export
+		.inport_external_connection_export      (<connected-to-inport_external_connection_export>),      //      inport_external_connection.export
+		.hexdisplay2_external_connection_export (<connected-to-hexdisplay2_external_connection_export>), // hexdisplay2_external_connection.export
+		.ledgreen_external_connection_export    (<connected-to-ledgreen_external_connection_export>),    //    ledgreen_external_connection.export
+		.ledred_external_connection_export      (<connected-to-ledred_external_connection_export>),      //      ledred_external_connection.export
+		.buttons_external_connection_export     (<connected-to-buttons_external_connection_export>)      //     buttons_external_connection.export
+	);
+
